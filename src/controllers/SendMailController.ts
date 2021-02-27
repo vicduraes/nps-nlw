00a55d522e3a3944a8beb72f5d12a4458b163f14
vicduraes <duraes.vic@gmail.com>
2021-02-27 20:44:16 -0300

    chore: create html template for emails

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import { resolve } from 'path';
+
 import SurveysRepository from '../repositories/SurveysRepository';
 import SurveysUserRepository from '../repositories/SurveysUserRepository';
 import UsersRepository from '../repositories/UsersRepository';
@@ -12,35 +14,39 @@ class SendMailController {
     const surveyRepository = getCustomRepository(SurveysRepository);
     const surveysUserRepository = getCustomRepository(SurveysUserRepository);
 
-    const userAlreadyExists = await usersRepository.findOne({ email });
-    const surveyAlreadyExists = await surveyRepository.findOne({
+    const user = await usersRepository.findOne({ email });
+    const survey = await surveyRepository.findOne({
       id: survey_id,
     });
 
-    if (!userAlreadyExists) {
+    if (!user) {
       return response.status(400).json({
         error: 'User does not exists.',
       });
     }
 
-    if (!surveyAlreadyExists) {
+    if (!survey) {
       return response.status(400).json({
         error: 'Survey does not exists.',
       });
     }
 
     const surveyUser = surveysUserRepository.create({
-      user_id: userAlreadyExists.id,
+      user_id: user.id,
       survey_id,
     });
 
     await surveysUserRepository.save(surveyUser);
 
-    await SendMailService.execute(
-      email,
-      surveyAlreadyExists.title,
-      surveyAlreadyExists.description
-    );
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
+
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+    };
+
+    await SendMailService.execute(email, survey.title, variables, npsPath);
 
     return response.status(201).json(surveyUser);
   }
--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,4 +1,6 @@
 import nodemailer, { Transporter } from 'nodemailer';
+import fs from 'fs';
+import handleBars from 'handlebars';
 
 class SendMailService {
   private client: Transporter;
@@ -19,11 +21,17 @@ class SendMailService {
     });
   }
 
-  async execute(to: string, subject: string, body: string) {
+  async execute(to: string, subject: string, variables: object, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString('utf-8');
+
+    const mailTemplateParse = handleBars.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       subject,
-      html: body,
+      html,
       from: 'NPS <noreplay@nps.com.br>',
     });
 
